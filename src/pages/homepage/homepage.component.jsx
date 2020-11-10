6f4e35d574afe520f240602d9e1e669a81f1fc82
Doddy Amijaya <doddyillona@gmail.com>
2020-11-10 08:37:38 -0600

    deconstruct to bare minimum again. created homepages components and added sass file

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
-
-import Directory from '../../components/directory/directory.component';
-
-import './homepage.styles.scss';
+import './homepage.styles.scss'
 
 const HomePage = () => (
   <div className='homepage'>
-    <Directory />
+    <div className='directory-menu'> 
+      <div className='menu-item'>
+        <div className='content'>
+          <h1>TOPI</h1>
+          <span>SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className='menu-item'>
+        <div className='content'>
+          <h1>JAKET</h1>
+          <span>SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className='menu-item'>
+        <div className='content'>
+          <h1>SEPATU</h1>
+          <span>SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className='menu-item'>
+        <div className='content'>
+          <h1>WOMEN</h1>
+          <span>SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className='menu-item'>
+        <div className='content'>
+          <h1>MENS</h1>
+          <span>SHOP NOW</span>
+          </div>
+        </div>
+    </div>
   </div>
+
+  
 );
 
 export default HomePage;

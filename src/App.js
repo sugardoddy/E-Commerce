9f65e2062b0c5c05410580afd850522319d25619
Doddy Amijaya <doddyillona@gmail.com>
2020-11-11 07:53:40 -0600

    add menu items comp to navigate withRouter Higher Order Comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component';
+const hatsPage = (props) => {
+  console.log(props);
+  return(
+    <h1> TOPI PAGE </h1>
+  );
+  
+  }
+
+  const jaketPage = (props) => {
+    console.log(props);
+    return(
+      <h1> JAKET PAGE </h1>
+    );
+    
+    }
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Switch>
+      <Route exact path='/' component={ HomePage } />
+      <Route exact path='/topi' component={ hatsPage } />
+      <Route path='/topi/:topicsId' component={ hatsPage } />
+      <Route path='/jaket' component={ jaketPage } />
+    </Switch>
+      
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,29 +13,34 @@ class Directory extends React.Component {
         {
           title: 'topi',
           imageUrl: 'https://i.ebayimg.com/images/g/wMoAAOSw-RhacJBY/s-l300.jpg',
-          id: 1
+          id: 1,
+          linkUrl: 'topi'
         },
         {
           title: 'jaket',
           imageUrl: 'https://assets.adidas.com/images/w_280,h_280,f_auto,q_auto:sensitive/bd751cd6c6ec4db1b759ac4101879926_9366/ctn-event-hood-pulovr.jpg',
-          id: 2
+          id: 2,
+          linkUrl: 'jaket'
         },
         {
           title: 'sepatu',
           imageUrl: 'https://wallpaperaccess.com/full/830639.jpg',
-          id: 3
+          id: 3,
+          linkUrl: 'sepatu'
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: 'womens'
         },
         {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: 'mens'
         }
       ]
     }
@@ -45,8 +50,8 @@ class Directory extends React.Component {
     return(
       <div className='directory-menu'>
       {
-        this.state.sections.map(({title, imageUrl, size, id}) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size = {size} />
+        this.state.sections.map(({id, ...otherSectionProps}) => (
+          <MenuItem key={ id } {...otherSectionProps} />
         ))
       } 
       </div>
